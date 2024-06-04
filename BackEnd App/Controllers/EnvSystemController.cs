6800b88d3d0acc3cd66ce6cee47f9ad34ab3950a
Alexandru-Marian Buza <alexandrumarianbuza@gmail.com>
2024-06-04 15:02:07 +0300

    Add test controller

---
+++ BackEnd App/Controllers/EnvSystemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_App.Controllers
+{
+    [ApiController]
+    [Route("/env")]
+    public class EnvSystemController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return Environment.GetEnvironmentVariable("testENV");
+        }
+    }
+}
