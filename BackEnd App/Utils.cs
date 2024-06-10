3fd1c174f2ed67977d74650dfbf2a6ca1a858e89
Catalin Catta <catalincatta@yahoo.com>
2024-06-10 18:19:38 +0300

    new class for utils

---
+++ BackEnd App/Utils.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_App
+{
+    public static class Utils
+    {
+        public static async Task<List<T>> GetMultipleElementsByValue<T>(
+            DbSet<T> context,
+            int number,
+            int size
+        )
+            where T : class =>
+            (
+                number == 0 || size == 0
+                    ? await context.ToListAsync()
+                    : await context.Take(size).Skip(size * (number - 1)).Take(size).ToListAsync()
+            );
+    }
+}
