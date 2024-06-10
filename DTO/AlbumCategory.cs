da903df24859580dbd3940acb72e56a801ca458f
Catalin Catta <catalincatta@yahoo.com>
2024-06-10 18:15:52 +0300

    DTO classes

---
+++ DTO/AlbumCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO
+{
+    public class AlbumCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
--- DTO/enums.cs
+++ DTO/enums.cs
@@ -1,8 +1,49 @@
-﻿namespace DTO
+﻿using System.ComponentModel;
+
+namespace DTO
 {
     public enum FileType
     {
+        [Description("unknowns")]
+        Unknown,
+
+        [Description("images")]
         Image,
+
+        [Description("videos")]
         Video
+    }
+
+    public enum InfoName
+    {
+        [Description("userInfo.name")]
+        UserName,
+
+        [Description("userInfo.adress")]
+        UserAdress,
+
+        [Description("userInfo.phoneNrs")]
+        UserPhoneNrs,
+
+        [Description("userInfo.emails")]
+        UserEmails,
+
+        [Description("userInfo.name")]
+        UserSocialMediaLinks,
+
+        [Description("homeDescription.name")]
+        HomeDescriptionName,
+
+        [Description("homeDescription.title")]
+        HomeDescriptionTitle,
+
+        [Description("homeDescription.text")]
+        HomeDescriptionText,
+
+        [Description("homeDescription.backGroundFileId")]
+        HomeDescriptionBackgroundFileId,
+
+        [Description("otherInfo.logo")]
+        Logo,
     }
 }
